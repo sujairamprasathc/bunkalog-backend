861e9cdbaf7bd8b3d6a0775cfecd64c4e2886fda
Sujai Ram Prasath C <20027296+sujairamprasathc@users.noreply.github.com>
2021-09-16 00:06:52 +0530

    Move data storage to MongoDB

--- app.py
+++ app.py
@@ -2,43 +2,41 @@
 # encoding: utf-8
 import json
 import uuid
+import pymongo
 from datetime import datetime
 from flask import Flask, request
 
 app = Flask(__name__)
 
-data = {}
-loggedInUserList = {}
-
-
-def redirectScript():
-    return '<script>window.location.replace("http://localhost:3000/")</script>'
-
-
 
 
 @app.route('/')
 def index():
-    print(data, loggedInUserList)
-    print('DEBUG:', request.cookies)
-    if 'bunkalog_session_id' in request.cookies:
-        print(request.cookies.get('bunkalog_session_id'))
-    return json.dumps(data) 
+    res = collection1.find({})
+    all_user_data = []
+    for user_data in res:
+        all_user_data.append(user_data)
+    return json.dumps(all_user_data)
 
 
 
 @app.route('/loggedInUserList')
 def getLoggedInUserList():
-    print(loggedInUserList)
-    return json.dumps(loggedInUserList, default=str)
+    res = collection2.find({})
+    logged_in_users_list = []
+    for user in res:
+        logged_in_users_list.append(user)
+    return json.dumps(logged_in_users_list, default=str)
 
 
 
 @app.route('/user')
 def getUserData():
     session_id = request.args.get('session_id')
-    user_id = loggedInUserList[session_id][0]
-    return json.dumps(data[user_id])
+    res = collection2.find_one({'_id': session_id})
+    user_id = res["user_id"]
+    user_data = collection1.find_one({'_id': user_id})
+    return json.dumps(user_data["data"])
 
 
 
@@ -46,11 +44,16 @@ def getUserData():
 def getCourseData():
     courseCode = request.args.get('courseCode')
     session_id = request.args.get('session_id')
-    user_id = loggedInUserList[session_id][0]
+    
+    res = collection2.find_one({'_id': session_id})
+    user_id = res["user_id"]
+
+    user_data = collection1.find_one({'_id': user_id})
+
     response = {}
-    for i in data[user_id]:
-        if i['courseCode'] == courseCode:
-            response = i
+    for course in user_data["data"]:
+        if course['courseCode'] == courseCode:
+            response = course
             break
     return json.dumps(response)
 
@@ -62,14 +65,15 @@ def login():
         #print(request.json['response']['googleId'])
 
         user_id = request.json['response']['googleId']
-        if user_id not in data:
-            data[user_id] = []
+
+        res = collection1.find_one({'_id': user_id})
+        if res == None:
+            res = collection1.insert_one({'_id': user_id, 'data': []})
 
         session_id = uuid.uuid4()
-        loggedInUserList[str(session_id)] = [user_id, datetime.now()]
+        res = collection2.insert_one({'_id': str(session_id), 'user_id': user_id})
 
-        print(user_id, str(session_id))
-
+        #print(user_id, str(session_id))
         return json.dumps(str(session_id))
 
 
@@ -78,7 +82,7 @@ def login():
 def logout():
     if request.method == 'POST':
         session_id = request.headers["Bunkalog-Session-Id"]
-        del loggedInUserList[session_id]
+        res = collection2.delete_one({'_id': session_id})
     return 'logout request received'
 
 
@@ -89,13 +93,21 @@ def addClass():
         print(request.json)
         request.json
         session_id = request.headers["Bunkalog-Session-Id"]
-        user_id = loggedInUserList[session_id][0]
+
+        res = collection2.find_one({'_id': session_id})
+        user_id = res["user_id"]
+        
         new_class_details = request.json
+        
         # Perform validation
         new_class_details['classesAttended'] = int(new_class_details['classesAttended'])
         new_class_details['totalClasses'] = int(new_class_details['totalClasses'])
-        data[user_id].append(new_class_details)
-        return redirectScript()
+
+        res = collection1.find_one({'_id': user_id})
+        res["data"].append(new_class_details)
+        res = collection1.update_one({'_id': user_id}, {"$set": res})
+        
+        return 'success'
 
 
 
@@ -105,12 +117,19 @@ def attendClass():
         print(request.json)
         classVal = request.json['courseCode']
         session_id = request.headers["Bunkalog-Session-Id"]
-        user_id = loggedInUserList[session_id][0]
-        for i in data[user_id]:
-            if(i['courseCode']==classVal):
-                i['classesAttended']=i['classesAttended']+1
-                i['totalClasses']=i['totalClasses']+1
-        return redirectScript()
+        res = collection2.find_one({'_id': session_id})
+        user_id = res["user_id"]
+
+        res = collection1.find_one({'_id': user_id})
+
+        for course in res["data"]:
+            if course['courseCode'] == classVal:
+                course['classesAttended'] = course['classesAttended'] + 1
+                course['totalClasses'] = course['totalClasses'] + 1
+
+        res = collection1.update_one({'_id': user_id}, {"$set": res})
+
+        return 'success'
 
 
 
@@ -120,13 +139,37 @@ def bunkClass():
         print(request.json)
         classVal = request.json['courseCode']
         session_id = request.headers["Bunkalog-Session-Id"]
-        user_id = loggedInUserList[session_id][0]
-        for i in data[user_id]:
-            if(i['courseCode']==classVal):
-                i['totalClasses']=i['totalClasses']+1
-        return redirectScript()
+        res = collection2.find_one({'_id': session_id})
+        user_id = res["user_id"]
+
+        res = collection1.find_one({'_id': user_id})
+
+        for course in res["data"]:
+            if course['courseCode'] == classVal:
+                course['totalClasses'] = course['totalClasses'] + 1
+
+        res = collection1.update_one({'_id': user_id}, {"$set": res})
+
+        return 'success'
 
 
 
 if __name__=='__main__':
-        app.run(host="127.0.0.1")
+    with open('.env') as f:
+        password = f.readline()
+        #print(password, len(password), bytes(password, 'utf-8'))
+    client = pymongo.MongoClient("mongodb://csrp:" + password[:-1] + "@valhalla-shard-00-00.7f9jf.mongodb.net:27017,valhalla-shard-00-01.7f9jf.mongodb.net:27017,valhalla-shard-00-02.7f9jf.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-13itpy-shard-0&authSource=admin&retryWrites=true&w=majority")
+    db = client.test
+
+    global databases
+    global collection1
+    databases = client.bunkalog
+    collection1 = databases.attendance_log
+    collection2 = databases.session_log
+    
+    """#res = collection1.insert_one({'_id': '101', 'user_id': '100'})
+    res = collection1.find({})
+    for doc in res:
+        print(doc)"""
+
+    app.run(host="127.0.0.1")
